ac0ec8fd3f02e2bf419c04765c89e3bc4a53b89c
kwpresto <kpresto21@yahoo.com>
2016-04-28 00:20:53 -0700

    v1.1.3 clean up

--- src/ai.js
+++ src/ai.js
@@ -1,6 +1,6 @@
 var AI = {
     show: function(num){
-        var mu = "<div class='fadeBox'><div class='spinner" + num + "'></div><a class='close' href='javascript:AI.hide()'>Done</a></div>";
+        var mu = "<div class='fadeBox'><div class='spinner s00" + num + "'></div><a class='close' href='javascript:AI.hide()'>Done</a></div>";
         document.body.innerHTML += mu;
     },
     hide: function(){
